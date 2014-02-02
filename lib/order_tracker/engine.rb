cf4125dedb2be692cc9417b223543e48fd84e499
Gautam Naroji <gautamnaroji@gautams-mbp.home>
2014-02-02 17:16:18 +0530

    switched unit tests with rspec

--- lib/order_tracker/engine.rb
+++ lib/order_tracker/engine.rb
@@ -1,5 +1,7 @@
 module OrderTracker
   class Engine < ::Rails::Engine
     isolate_namespace OrderTracker
+    config.generators.integration_tool :rspec
+    config.generators.test_framework   :rspec
   end
 end
